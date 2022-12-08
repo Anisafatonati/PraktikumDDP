1440ade04328eadb99a9407d269a916d6932d35d
Anisafatonati <114457553+Anisafatonati@users.noreply.github.com>
2022-12-08 07:40:50 +0700

    Add files via upload

---
+++ DDP/pertemuan1/perulangan.py
@@ -0,0 +1,3 @@
+for i in range(3, 12, 2): #nilai 1 : dari, nilai 2 : samapai, nilai 3 : selisih
+    print(i)
+print("Terimakasih")
---
+++ DDP/pertemuan1/variable.py
@@ -0,0 +1,7 @@
+namaLengkap = "Anisa Fatonati"  # string
+asal = "Lampung"  # string
+umur = 18  # integer
+hobi = "makan"  # string
+isStudent = True  # boolean
+
+print('Hallo nama saya', namaLengkap)
