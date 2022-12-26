5b5889e3474a9e1418cc936642224a3bbabc15a5
Anisafatonati <114457553+Anisafatonati@users.noreply.github.com>
2022-12-26 11:26:24 +0700

    Add files via upload

---
+++ pertemuan2/tugas2.py
@@ -0,0 +1,9 @@
+#input
+a = float(input("Masukan banyaknya angka :")) 
+b = float(input("masukan angka:"))
+c = float(input("masukan angka:"))
+d = float(input("masukan angka:"))
+e = float(input("masukan angka:"))
+f = float(input("masukan angka:"))
+hasil = (10+12.5+(-4.2)+123.9+(-3))/5 
+print("hasil rata-rata perhitungan nilai adalah: " +str(hasil))
---
+++ pertemuan2/tugas3.py
@@ -0,0 +1,6 @@
+J = int(input("masukan input:"))
+X = str("#")
+for i in range(0,J):
+    for j in range(0,5):
+        print(X,end=" ")
+    print()
